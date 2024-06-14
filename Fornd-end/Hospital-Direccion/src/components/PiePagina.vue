f44303b8bdf81629c5304cf735785a095bff2744
Your Name <210237@utxicotepec.edu.com>
2024-06-13 23:32:03 -0600

    se modifico el footer con colores en la estrucutura del texto

--- Fornd-end/Hospital-Direccion/src/components/PiePagina.vue
+++ Fornd-end/Hospital-Direccion/src/components/PiePagina.vue
@@ -1,21 +1,21 @@
 <template>
     
-<footer class="bg-black rounded-lg shadow m-4 dark:bg-slate-800">
+<footer class="bg-slate-800 rounded-lg shadow m-4 slate:bg-slate-800">
     <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span class="text-sm text-gray-500 sm:text-center white:text-gray-400 text-blout">© 2024 <a href="https://flowbite.com/" class="hover:underline">PrivilegeCare™</a>. Hospital de Especialidades.
+      <span class="text-sm text-slate-200 sm:text-center white:text-gray-400 text-blout font-bold">© 2024 <a href="https://flowbite.com/" class="hover:underline">PrivilegeCare™</a>. Hospital de Especialidades.
     </span>
-    <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+    <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-slate-200 dark:text-whithe-400 sm:mt-0">
         <li>
-            <a href="#" class="hover:underline me-4 md:me-6">Nosotros</a>
+            <a href="#" class="hover:underline me-4 md:me-6 font-bold">Nosotros</a>
         </li>
         <li>
-            <a href="#" class="hover:underline me-4 md:me-6">Politica de Privacidad</a>
+            <a href="#" class="hover:underline me-4 md:me-6 font-bold">Politica de Privacidad</a>
         </li>
         <li>
-            <a href="#" class="hover:underline me-4 md:me-6">Estatutos</a>
+            <a href="#" class="hover:underline me-4 md:me-6 font-bold">Estatutos</a>
         </li>
         <li>
-            <a href="#" class="hover:underline">Contacto</a>
+            <a href="#" class="hover:underline font-bold">Contacto</a>
         </li>
     </ul>
     </div>
